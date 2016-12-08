3bfdd2095f2215f455f3a1d030680c81cf6a0aa5
HarryJamesDavies <daviesharry1@gmail.com>
2016-12-08 11:31:43 +0000

    You can drive around in a basic car and explore the map, falling into the water plane will kill you if you go deep enough. If you flip press Q to reset yourself. L for toggle lights and H for horn.

---
+++ CarGame/Assets/Scripts/KillBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour
+{
+    public GameObject m_spawnPoint;
+    public Quaternion m_spawnRotation;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Car"))
+        {
+            collision.transform.position = m_spawnPoint.transform.position;
+            collision.transform.rotation = m_spawnRotation;
+        }
+    }
+}
