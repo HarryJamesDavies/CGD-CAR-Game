099cb9cdcf0ef41dd860edc19565080dbb185c9a
HarryJamesDavies <daviesharry1@gmail.com>
2016-12-09 13:38:49 +0000

    Only sections within a set distance of the event will turn on.

--- CarGame/Assets/Scripts/DeadCarManager.cs
+++ CarGame/Assets/Scripts/DeadCarManager.cs
@@ -4,19 +4,130 @@ using System.Collections.Generic;
 
 public class DeadCarManager : MonoBehaviour {
 
+    public static DeadCarManager m_instance = null;
     public List<GameObject> m_sections;
+    public float m_minimumDistance;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        for (int i = 0; i <= transform.childCount; i++)
+        if (m_instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_instance = this;
+        }
+
+        for (int i = 0; i <= transform.childCount - 1; i++)
         {
             m_sections.Add(transform.GetChild(i).gameObject);
         }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if(EventManager.m_instance.m_stateChanged)
+        {
+            OnEventEnd();
+
+            OnEventBegin();
+        }
+
+        OnEventUpdate();
 	}
+
+    public void OnEventBegin()
+    {
+        switch (EventManager.m_instance.m_currentState)
+        {
+            case EventManager.Events.FREEROAM:
+                {
+                    break;
+                }
+            case EventManager.Events.DRIVEANDSEEK:
+                {
+                    SetSections();
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    public void OnEventUpdate()
+    {
+        switch (EventManager.m_instance.m_currentState)
+        {
+            case EventManager.Events.FREEROAM:
+                {
+                    break;
+                }
+            case EventManager.Events.DRIVEANDSEEK:
+                {
+
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    public void OnEventEnd()
+    {
+        switch (EventManager.m_instance.m_prevState)
+        {
+            case EventManager.Events.FREEROAM:
+                {
+                    break;
+                }
+            case EventManager.Events.DRIVEANDSEEK:
+                {
+                    ResetSections();
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    void SetSections()
+    {
+        foreach(GameObject sections in m_sections)
+        {
+            if(m_minimumDistance >= Vector3.Distance(EventManager.m_instance.m_eventLocation, sections.transform.position))
+            {
+                sections.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void ResetSections()
+    {
+        foreach (GameObject sections in m_sections)
+        {
+            sections.gameObject.SetActive(false);
+        }
+    }
+
+    /*
+        if (Input.GetKeyDown("e") || Input.GetButtonDown("P" + _controller + ("-Circle(PS4)")))
+        {
+            EventManager.m_instance.m_eventLocation = this.gameObject.transform.position;
+            EventManager.m_instance.m_currentState = EventManager.Events.DRIVEANDSEEK;
+        }
+
+        if (Input.GetKeyDown("r") || Input.GetButtonDown("P" + _controller + ("-Circle(PS4)")))
+        {
+            EventManager.m_instance.m_currentState = EventManager.Events.FREEROAM;
+        }
+    */
 }
--- CarGame/Assets/Scripts/EventManager.cs
+++ CarGame/Assets/Scripts/EventManager.cs
@@ -3,14 +3,43 @@ using System.Collections;
 
 public class EventManager : MonoBehaviour
 {
+    public static EventManager m_instance = null;
+
+    public enum Events
+    {
+        FREEROAM = 0,
+        DRIVEANDSEEK = 1,
+        Length
+    };
+
+    public Events m_currentState = Events.FREEROAM;
+    public Events m_prevState = Events.FREEROAM;
+    public bool m_stateChanged = false;
+
+    public Vector3 m_eventLocation;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+	    if(m_instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_instance = this;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        m_stateChanged = false;
+
+        if (m_currentState != m_prevState)
+        {
+            m_stateChanged = true;
+            m_prevState = m_currentState;
+        }
 	}
 }
