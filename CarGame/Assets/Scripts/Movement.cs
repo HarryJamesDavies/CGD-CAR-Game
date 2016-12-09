7d2eba7356b215e0d71325b9d6c1ae3cae838ec0
Adster94 <adam.moorey@hotmail.co.uk>
2016-12-09 11:38:58 +0000

    PS4 controls are successfully integrated with movement, lights and horn controls. Keyboard controls are still in the game.

--- CarGame/Assets/Scripts/Movement.cs
+++ CarGame/Assets/Scripts/Movement.cs
@@ -78,22 +78,59 @@ public class Movement : MonoBehaviour {
             default:
                 break;
         }
+    }
 
-        if (Input.GetKeyDown("q"))
+    void PlayerMovement(int _controller)
+    {
+        if (Input.GetKeyDown("w") || Input.GetAxis("P" + _controller + "-R2(PS4)") > -1.0f)
+        {
+            forward = true;
+        }
+        if (Input.GetKeyUp("w") || Input.GetAxis("P" + _controller + "-R2(PS4)") == -1.0f)
+        {
+            forward = false;
+        }
+        if (Input.GetKeyDown("s") || Input.GetAxis("P" + _controller + "-L2(PS4)") > -1.0f)
+        {
+            backward = true;
+        }
+        if (Input.GetKeyUp("s") || Input.GetAxis("P" + _controller + "-L2(PS4)") == -1.0f)
+        {
+            backward = false;
+        }
+
+        if (Input.GetKeyDown("d") || Input.GetAxis("P" + _controller + ("-LeftJoystickX(PS4)")) > 0.5f)
+        {
+            right = true;
+        }
+        if (Input.GetKeyUp("d") || Input.GetAxis("P" + _controller + ("-LeftJoystickX(PS4)")) < 0.5f)
+        {
+            right = false;
+        }
+        if (Input.GetKeyDown("a") || Input.GetAxis("P" + _controller + ("-LeftJoystickX(PS4)")) < -0.5f)
+        {
+            left = true;
+        }
+        if (Input.GetKeyUp("a") || Input.GetAxis("P" + _controller + ("-LeftJoystickX(PS4)")) > -0.5f)
+        {
+            left = false;
+        }
+
+        if (Input.GetKeyDown("q") || Input.GetButtonDown("P" + _controller + ("-Triangle(PS4)")))
         {
             transform.position += Vector3.up * 2.5f;
             transform.rotation = startingRotation;
         }
 
-        if (Input.GetKeyDown("l"))
+        if (Input.GetKeyDown("l") || Input.GetButtonDown("P" + _controller + ("-Square(PS4)")))
         {
-            foreach(GameObject lights in m_lights)
+            foreach (GameObject lights in m_lights)
             {
                 lights.SetActive(!lights.activeInHierarchy);
             }
         }
 
-        if (Input.GetKeyDown("h"))
+        if (Input.GetKeyDown("h") || Input.GetButtonDown("P" + _controller + ("-Circle(PS4)")))
         {
             int sound = Random.Range(0, m_hornSounds.Count);
             m_audioSource.clip = m_hornSounds[sound];
@@ -107,43 +144,5 @@ public class Movement : MonoBehaviour {
 
         xspeep *= friction;
         transform.Translate(Vector3.forward * -xspeep);
-
-    }
-
-    void PlayerMovement(int _controller)
-    {
-        if (Input.GetKeyDown("w"))
-        {
-            forward = true;
-        }
-        if (Input.GetKeyUp("w"))
-        {
-            forward = false;
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            backward = true;
-        }
-        if (Input.GetKeyUp("s"))
-        {
-            backward = false;
-        }
-
-        if (Input.GetKeyDown("d"))
-        {
-            right = true;
-        }
-        if (Input.GetKeyUp("d"))
-        {
-            right = false;
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            left = true;
-        }
-        if (Input.GetKeyUp("a"))
-        {
-            left = false;
-        }
     }
 }
