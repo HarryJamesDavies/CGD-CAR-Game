285ae0ad4ad641410557ccd8888b351ffa4092f8
Falsan <sam.britnell117@gmail.com>
2016-12-18 10:59:48 +0000

    Gamelogic and manager is now in. Works, with some missing features.

---
+++ CarGame/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+    public float targetTime = 60.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+
+        GetComponent<TextMesh>().text = string.Format("{0:N2}", targetTime);
+    }
+    
+    void timerEnded()
+    {
+        //do your stuff here.
+    }
+}
+
