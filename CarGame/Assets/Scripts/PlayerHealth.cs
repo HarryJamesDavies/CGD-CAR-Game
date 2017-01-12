11b5469b73700cb4bd058090c7cc9ec0a1cde8f5
jplant20 <Josephsp@hotmail.co.uk>
2017-01-12 17:26:35 +0000

    Simple Health Bar made where health (100) reduces by 2 every second. Made a new Prefab of P1 as well but kept original just in case.

---
+++ CarGame/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public float max_Health = 100f;
+	public float cur_Health = 0f;
+	public GameObject healthbar;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cur_Health = max_Health;
+		InvokeRepeating ("decreasehealth", 1f , 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void decreasehealth()
+	{
+		cur_Health -= 2f;
+		float calc_Health = cur_Health / max_Health;
+		SetHealthBar (calc_Health);
+	}
+
+	public void SetHealthBar(float myHealth)
+	{
+		//myHealth value 0-1
+		healthbar.transform.localScale = new Vector3(Mathf.Clamp(myHealth, 0f , 1f) , healthbar.transform.localScale.y , healthbar.transform.localScale.z);
+	}
+}
