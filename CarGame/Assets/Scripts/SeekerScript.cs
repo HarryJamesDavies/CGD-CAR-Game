1c2a53e676d91861d4a6eb0edb671c62954fce28
MattWY <matthew.wynter@btinternet.com>
2017-01-15 18:16:30 +0000

    Detection for hiding and seeking.

--- CarGame/Assets/Scripts/SeekerScript.cs
+++ CarGame/Assets/Scripts/SeekerScript.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class SeekerScript : MonoBehaviour
 {
     int m_playerNumber;
+    int m_damageApplied = 0;
+    bool m_lights = true;
+
+    float timer = 5.0f;
+    bool m_hiderHit = false;
 
     public string m_hiderTag;
 
@@ -15,49 +20,134 @@ public class SeekerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Seeker: " + m_playerNumber);
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        //if damage is required to be taken
-        if (ControllerManager.m_instance.m_useController)
+        if (m_damageApplied > 0 && timer > 0.0f)
         {
-            if (Input.GetButtonDown("P" + m_playerNumber + ("-X(PS4)")))
-            {
-                if (m_hiderTag == other.gameObject.tag)
-                {
-                    //take damage for the hider car
-                    Debug.Log("Hider Take Damage");
-                    other.gameObject.GetComponent<PlayerHealth>().decreasehealth();
-                }
-                else
-                {
-                    //seeker take damage
-                    Debug.Log("Seeker Take Damage");
-                    transform.GetComponent<PlayerHealth>().decreasehealth();
-                }
-            }
+            timer -= Time.deltaTime;
+            Debug.Log(timer);
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.V))
+            m_damageApplied = 0;
+            timer = 5.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Collider[] m_itemsInRange = Physics.OverlapSphere(transform.parent.position, 2);
+            int i = 0;
+            while(i < m_itemsInRange.Length)
             {
-                if (m_hiderTag == other.gameObject.tag)
+                if(m_itemsInRange[i].tag == m_hiderTag)
                 {
-                    //take damage for the hider car
-                    Debug.Log("Hider Take Damage");
-                    other.gameObject.GetComponent<PlayerHealth>().decreasehealth();
-                    //m_damageApplied++;
+                    m_hiderHit = true;
+                    DamageHider(m_itemsInRange[i]);
+                 
                 }
-                else
-                {
-                    //seeker take damage
-                    Debug.Log("Seeker Take Damage");
-                    transform.parent.GetComponent<PlayerHealth>().decreasehealth();
-                }
+                i++;
             }
+           
+            if(!m_hiderHit)
+            {
+                DamageSeeker();
+        
+            }
+            else
+            {
+                m_hiderHit = false;
+            }
+
+
         }
 
     }
+
+    void TurnLightsOn()
+    {
+        if (!m_lights)
+        {
+            foreach (GameObject lights in transform.parent.GetComponent<Movement>().m_lights)
+            {
+                lights.SetActive(lights.activeInHierarchy);
+            }
+            m_lights = true;
+        }
+    }
+
+    void TurnLightsOff()
+    {
+        if (m_lights)
+        {
+            foreach (GameObject lights in transform.parent.GetComponent<Movement>().m_lights)
+            {
+                lights.SetActive(!lights.activeInHierarchy);
+            }
+            m_lights = false;
+        }
+    }
+
+    void DamageSeeker()
+    {
+        if (m_damageApplied == 0)
+        {
+            transform.parent.GetComponent<PlayerHealth>().decreasehealth();
+            m_damageApplied++;
+        }
+    }
+
+    void DamageHider(Collider other)
+    {
+        if (m_damageApplied == 0)
+        {
+            other.gameObject.GetComponent<PlayerHealth>().decreasehealth();
+            m_damageApplied++;
+        }
+    }
+
+    //void OnTriggerStay(Collider other)
+    //{
+    //    //if damage is required to be taken
+    //    if (ControllerManager.m_instance.m_useController)
+    //    {
+    //        if (Input.GetButtonDown("P" + m_playerNumber + ("-X(PS4)")))
+    //        {
+    //            if (m_hiderTag == other.gameObject.tag)
+    //            {
+    //                //take damage for the hider car
+    //                Debug.Log("Hider Take Damage");
+    //                other.gameObject.GetComponent<PlayerHealth>().decreasehealth();
+    //            }
+    //            else
+    //            {
+    //                //seeker take damage
+    //                Debug.Log("Seeker Take Damage");
+    //                transform.GetComponent<PlayerHealth>().decreasehealth();
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.V))
+    //        {
+                
+    //                if (m_hiderTag == other.gameObject.tag)
+    //                {                     
+    //                    //take damage for the hider car
+    //                    Debug.Log("Hider Take Damage");
+    //                    DamageHider(other);
+                    
+    //                }
+    //                else
+    //                {
+    //                    //seeker take damage                       
+    //                    Debug.Log("Seeker Take Damage");
+    //                    DamageSeeker();
+                       
+    //                }
+                
+            
+    //        }
+    //    }
+
+    //}
 }
+
