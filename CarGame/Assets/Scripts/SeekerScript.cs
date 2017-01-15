07a46bf0ac2046b05ac33fc931871e08b3663472
Adster94 <adam.moorey@hotmail.co.uk>
2017-01-15 13:28:52 +0000

    Minor update to SeekerScript with PS4 controller support.

--- CarGame/Assets/Scripts/SeekerScript.cs
+++ CarGame/Assets/Scripts/SeekerScript.cs
@@ -6,6 +6,8 @@ public class SeekerScript : MonoBehaviour {
     Car m_car;
     Car m_hidercar;
 
+    int m_playerNumber;
+
     void Start ()
     {
         m_car = gameObject.GetComponent<Car>();
@@ -14,27 +16,56 @@ public class SeekerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	
+        
 	}
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag != "EmptyCar")
+        /*if (other.gameObject.tag != "EmptyCar")
         {
             //if the car is a hider car
             m_hidercar = other.gameObject.GetComponent<Car>();
 
-            //if ( get the hidertag variable m_hidertag == true)
-            //{
-            //    //if damage is required to be taken
-            //    if (Input.GetKey(KeyCode.Z))
-            //    {
-            //        //take damage for the hider car 
-            //        Debug.Log("Take Damage");
-            //    }
-            //}
-        }
-    }
+            if (get the hidertag variable m_hidertag == true)
+            {
+                if (ControllerManager.m_instance.m_useController)
+                {
+                    int _playerNumber = 0;
 
-    
+                    switch (gameObject.tag)
+                    {
+                        case "Player1":
+                            _playerNumber = 1;
+                            break;
+                        case "Player2":
+                            _playerNumber = 2;
+                            break;
+                        case "Player3":
+                            _playerNumber = 3;
+                            break;
+                        case "Player4":
+                            _playerNumber = 4;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (Input.GetButtonDown("P" + _playerNumber + ("-X(PS4)")))
+                    {
+                        //take damage for the hider car 
+                        Debug.Log("Take Damage");
+                    }
+                }
+                else
+                {
+                    //if damage is required to be taken
+                    if (Input.GetKey(KeyCode.Z))
+                    {
+                        //take damage for the hider car 
+                        Debug.Log("Take Damage");
+                    }
+                }
+            }
+        }*/
+    }
 }
