d0bc5573e887b162a9ea2339a8a769681de7efc7
Adster94 <adam.moorey@hotmail.co.uk>
2017-01-17 10:51:21 +0000

    Updated four player controls for keyboard and controller. Also changed prefabs to start off with the lights off.

--- CarGame/Assets/Scripts/EventCollider.cs
+++ CarGame/Assets/Scripts/EventCollider.cs
@@ -5,64 +5,97 @@ public class EventCollider : MonoBehaviour {
 
     //check if player 1 has already pressed to play
     public bool m_firstPlayerPlaying = false;
+    public bool m_secondPlayerPlaying = false;
+    public bool m_thirdPlayerPlaying = false;
+    public bool m_fourthPlayerPlaying = false;
 
     private void OnTriggerStay(Collider other)
     {
+        switch(other.tag)
+        {
+            case "Player1":
+                if (m_firstPlayerPlaying == false)
+                {
+                    if (ControllerManager.m_instance.m_useController)
+                    {
+                        if (Input.GetButtonDown("P1-X(PS4)"))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                    else
+                    {
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                }
+                break;
+            case "Player2":
+                if (m_secondPlayerPlaying == false)
+                {
+                    if (ControllerManager.m_instance.m_useController)
+                    {
+                        if (Input.GetButtonDown("P2-X(PS4)"))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                    else
+                    {
+                        if (Input.GetKeyDown("."))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                }
+                break;
+            case "Player3":
+                if (m_thirdPlayerPlaying == false)
+                {
+                    if (ControllerManager.m_instance.m_useController)
+                    {
+                        if (Input.GetButtonDown("P3-X(PS4)"))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                    else
+                    {
+                        if (Input.GetKeyDown(KeyCode.Y))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                }
+                break;
+            case "Player4":
+                if (m_fourthPlayerPlaying == false)
+                {
+                    if (ControllerManager.m_instance.m_useController)
+                    {
+                        if (Input.GetButtonDown("P4-X(PS4)"))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                    else
+                    {
+                        if (Input.GetKeyDown(KeyCode.O))
+                        {
+                            SetDSM(other);
+                        }
+                    }
+                }
+                break;
+            default:
+                Debug.Log("Default for event trigger");
+                break;
+        }
         if (other.tag == "Player1")
         {
-            if (m_firstPlayerPlaying == false)
-            {
-                if (ControllerManager.m_instance.m_useController)
-                {
-                    if (Input.GetButtonDown("P1-X(PS4)")) 
-                    {
-                        Debug.Log(other.tag + " wants to connect!");
-
-                        //activates the laser and active player number
-                        PlayersConnected.pc_instance.IncrementPC();
-
-                        //only fire the beam if there is 1 player connected
-                        if (PlayersConnected.pc_instance.m_playersconnected > 0)
-                        {
-                            ShootBeam.sb_instance.DisplayLine();
-                        }
-
-                        ////start the event
-                        //GameModeManager.m_instance.m_currentEvent = GameModeManager.GameModeState.DRIVEANDSEEK;
-                        //Debug.Log("Drive and Seek started");
-                        //m_firstPlayerPlaying = true;
-
-                        GetComponent<DriveAndSeekMode>().m_active = true;
-                        GameModeManager.m_instance.m_currentGameMode = GetComponent<DriveAndSeekMode>();
-                        GameModeManager.m_instance.m_currentEvent = GameModeManager.GameModeState.DRIVEANDSEEK;
-                    }
-                }
-                else
-                {
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        Debug.Log(other.tag + " wants to connect!");
-
-                        //activates the laser and active player number
-                        PlayersConnected.pc_instance.IncrementPC();
-
-                        //only fire the beam if there is 1 player connected
-                        if (PlayersConnected.pc_instance.m_playersconnected > 0)
-                        {
-                            ShootBeam.sb_instance.DisplayLine();
-                        }
-
-                        ////start the event
-                        //GameModeManager.m_instance.m_currentEvent = GameModeManager.GameModeState.DRIVEANDSEEK;
-                        //Debug.Log("Drive and Seek started");
-                        //m_firstPlayerPlaying = true;
-
-                        GetComponent<DriveAndSeekMode>().m_active = true;
-                        GameModeManager.m_instance.m_currentGameMode = GetComponent<DriveAndSeekMode>();
-                        GameModeManager.m_instance.m_currentEvent = GameModeManager.GameModeState.DRIVEANDSEEK;
-                    }
-                }
-            }
+            
         }
 
         //not currently set up for the 2nd player
@@ -73,16 +106,26 @@ public class EventCollider : MonoBehaviour {
        
     }
 
-    // Use this for initialization
-    void Start ()
+    void SetDSM(Collider _other)
     {
-        
+        Debug.Log(_other.tag + " wants to connect!");
+
+        //activates the laser and active player number
+        PlayersConnected.pc_instance.IncrementPC();
+
+        //only fire the beam if there is 1 player connected
+        if (PlayersConnected.pc_instance.m_playersconnected > 0)
+        {
+            ShootBeam.sb_instance.DisplayLine();
+        }
+
+        ////start the event
+        //GameModeManager.m_instance.m_currentEvent = GameModeManager.GameModeState.DRIVEANDSEEK;
+        //Debug.Log("Drive and Seek started");
+        //m_firstPlayerPlaying = true;
+
+        GetComponent<DriveAndSeekMode>().m_active = true;
+        GameModeManager.m_instance.m_currentGameMode = GetComponent<DriveAndSeekMode>();
+        GameModeManager.m_instance.m_currentEvent = GameModeManager.GameModeState.DRIVEANDSEEK;
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-	
-  
-	}
 }
--- CarGame/Assets/Scripts/Hider.cs
+++ CarGame/Assets/Scripts/Hider.cs
@@ -9,6 +9,8 @@ public class Hider : MonoBehaviour {
     private Camera m_hiderCam;
     private bool m_hidingState = false;
 
+    int m_playerNumber;
+
     void Start()
     {
         EventManager.m_instance.SubscribeToEvent(Events.Event.DS_HIDING, EvFunc_HidingPhase);
@@ -16,6 +18,7 @@ public class Hider : MonoBehaviour {
 
         m_carMovement = gameObject.GetComponent<Movement>();
         m_car = gameObject.GetComponent<Car>();
+        m_playerNumber = m_car.m_playerNumber;
     }
 
     void EvFunc_HidingPhase()
@@ -31,128 +34,96 @@ public class Hider : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        SetLocation(m_car.m_playerNumber);
+        SetLocation();
 	}
 
-    void SetLocation(int _playerNumber)
+    void SetLocation()
     {
         if (ControllerManager.m_instance.m_useController)
         {
-            if (Input.GetButtonDown("P" + _playerNumber + ("-X(PS4)")))
+            switch(m_playerNumber)
             {
-                if (!m_carMovement.m_controls)
-                {
-                    m_carMovement.m_controls = true;
-                    m_car.ToggleCamera(true);
-                    m_carMovement.ToggleLights(true);
-
-                    if(m_hidingState)
+                case 1:
+                    if (Input.GetButtonDown("P1-X(PS4)"))
                     {
-                        EventManager.m_instance.AddEvent(Events.Event.DS_HIDERREADY);
+                        ToggleHide();
                     }
-                }
-                else
-                {
-                    m_carMovement.m_controls = false;
-                    m_car.ToggleCamera(false);
-                    m_carMovement.ToggleLights(false);
-                }
+                    break;
+                case 2:
+                    if (Input.GetButtonDown("P2-X(PS4)"))
+                    {
+                        ToggleHide();
+                    }
+                    break;
+                case 3:
+                    if (Input.GetButtonDown("P3-X(PS4)"))
+                    {
+                        ToggleHide();
+                    }
+                    break;
+                case 4:
+                    if (Input.GetButtonDown("P4-X(PS4)"))
+                    {
+                        ToggleHide();
+                    }
+                    break;
+                default:
+                    Debug.Log("Controller Hider Action Default");
+                    break;
             }
         }
         else
         {
-            switch (_playerNumber)
+            switch (m_playerNumber)
             {
                 case 1:
                     if (Input.GetKeyDown("z"))
                     {
-                        if (!m_carMovement.m_controls)
-                        {
-                            m_carMovement.m_controls = true;
-                            m_car.ToggleCamera(true);
-                            m_carMovement.ToggleLights(true);
-                        }
-                        else
-                        {
-                            m_carMovement.m_controls = false;
-                            m_car.ToggleCamera(false);
-                            m_carMovement.ToggleLights(false);
-
-                            if (m_hidingState)
-                            {
-                                EventManager.m_instance.AddEvent(Events.Event.DS_HIDERREADY);
-                            }
-                        }
+                        ToggleHide();
                     }
                     break;
                 case 2:
                     if (Input.GetKeyDown(KeyCode.RightShift))
                     {
-                        if (!m_carMovement.m_controls)
-                        {
-                            m_carMovement.m_controls = true;
-                            m_car.ToggleCamera(true);
-                            m_carMovement.ToggleLights(true);
-                        }
-                        else
-                        {
-                            m_carMovement.m_controls = false;
-                            m_car.ToggleCamera(false);
-                            m_carMovement.ToggleLights(false);
-
-                            if (m_hidingState)
-                            {
-                                EventManager.m_instance.AddEvent(Events.Event.DS_HIDERREADY);
-                            }
-                        }
+                        ToggleHide();
                     }
                     break;
                 case 3:
-                    if (Input.GetKeyDown("m"))
+                    if (Input.GetKeyDown("v"))
                     {
-                        if (!m_carMovement.m_controls)
-                        {
-                            m_carMovement.m_controls = true;
-                            m_car.ToggleCamera(true);
-                            m_carMovement.ToggleLights(true);
-                        }
-                        else
-                        {
-                            m_carMovement.m_controls = false;
-                            m_car.ToggleCamera(false);
-                            m_carMovement.ToggleLights(false);
-
-                            if (m_hidingState)
-                            {
-                                EventManager.m_instance.AddEvent(Events.Event.DS_HIDERREADY);
-                            }
-                        }
+                        ToggleHide();
                     }
                     break;
                 case 4:
-                    if (Input.GetKeyDown("v"))
+                    if (Input.GetKeyDown("m"))
                     {
-                        if (!m_carMovement.m_controls)
-                        {
-                            m_carMovement.m_controls = true;
-                            m_car.ToggleCamera(true);
-                            m_carMovement.ToggleLights(true);
-                        }
-                        else
-                        {
-                            m_carMovement.m_controls = false;
-                            m_car.ToggleCamera(false);
-                            m_carMovement.ToggleLights(false);
-
-                            if (m_hidingState)
-                            {
-                                EventManager.m_instance.AddEvent(Events.Event.DS_HIDERREADY);
-                            }
-                        }
+                        ToggleHide();
                     }
                     break;
                 default:
+                    Debug.Log("keyboard Hider Action Default");
                     break;
+            }
+        }
+    }
+
+    void ToggleHide()
+    {
+        if (!m_carMovement.m_controls)
+        {
+            m_carMovement.m_controls = true;
+            m_car.ToggleCamera(true);
+            m_carMovement.ToggleLights(true);
+        }
+        else
+        {
+            m_carMovement.m_controls = false;
+            m_car.ToggleCamera(false);
+            m_carMovement.ToggleLights(false);
+
+            if (m_hidingState)
+            {
+                EventManager.m_instance.AddEvent(Events.Event.DS_HIDERREADY);
             }
         }
     }
