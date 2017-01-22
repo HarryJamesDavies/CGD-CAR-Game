be665cae56e6134e068d47766ca0dad2d575e2fa
Falsan <sam.britnell117@gmail.com>
2017-01-22 11:27:26 +0000

    Hider can now spawn a decoy of themselves, doesn't despawn yet

---
+++ CarGame/Assets/Scripts/HiderAbilities.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiderAbilities : MonoBehaviour {
+
+    [SerializeField]
+    GameObject m_decoyReference;
+
+    GameObject m_decoy;
+
+	// Use this for initialization
+	void Start () {
+        m_decoyReference = gameObject;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Z))
+        {
+            SpawnDecoy();
+        }
+	}
+
+    void SpawnDecoy()
+    {
+        m_decoy = Instantiate(m_decoyReference);
+        
+        Destroy(m_decoy.GetComponentInChildren<Camera>().gameObject);
+        Destroy(m_decoy.GetComponent<Car>());
+        Destroy(m_decoy.GetComponent<Rigidbody>());
+        Destroy(m_decoy.GetComponent<Collider>());
+        Destroy(m_decoy.GetComponent<Movement>());
+        Destroy(m_decoy.GetComponent<Hider>());
+        Destroy(m_decoy.GetComponent<HiderAbilities>());
+    }
+}
