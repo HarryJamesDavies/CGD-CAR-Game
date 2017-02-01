c64accfc1a9b69cb4ca056f496ddde072d860bd4
MattWY <matthew.wynter@btinternet.com>
2017-02-01 20:33:36 +0000

    Adds decay to the chase breakers so they don't remain in the scene

---
+++ CarGame/Assets/Scripts/Runner/ChaseBreakerTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseBreakerTimer : MonoBehaviour {
+
+    float timer = 0.0f;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(this.gameObject.activeInHierarchy == true)
+        {
+            timer += Time.deltaTime;
+            destroyThisBreaker();
+        }
+	}
+
+    public void destroyThisBreaker()
+    {
+        if (timer >= 5.0f)
+        {
+            Destroy(gameObject);
+            timer = 0.0f;
+        }
+    }
+}
